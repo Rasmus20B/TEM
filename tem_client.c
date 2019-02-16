83e9f921439ad0361cceed3c681e1f8dcc163dde
jam <jmtateossian50b@gmail.com>
2019-02-16 18:40:11 +0000

    First TEM commit

--- tem_client.c
+++ tem_client.c
@@ -13,32 +13,32 @@
 #define PORT 7272
 int connect_to_server(char dest_ip_addr[15]) { //connects to user specified server
 	
-int client_socket;
-struct sockaddr_in server_addr;		//initialises a 'sockaddr_in' structure called 'server_addr'
-char buffer[256];	//used for storing messages before displaying to screen or sending to server
+	int client_socket;
+	struct sockaddr_in server_addr;		//initialises a 'sockaddr_in' structure called 'server_addr'
+	char buffer[256];	//used for storing messages before displaying to screen or sending to server
 
-system("clear");
+	system("clear");
 
-client_socket = socket(PF_INET, SOCK_STREAM, 0);	//creates the TCP client socket
-memset(&server_addr, '\0', sizeof(server_addr));	//overwrites any memory stored in 'server_addr' with zeros
+	client_socket = socket(PF_INET, SOCK_STREAM, 0);	//creates the TCP client socket
+	memset(&server_addr, '\0', sizeof(server_addr));	//overwrites any memory stored in 'server_addr' with zeros
 	
-server_addr.sin_family=AF_INET;		// specifies family of addresses (IPv4)
-server_addr.sin_port=htons(PORT);	// specifies port the server will be using 
-server_addr.sin_addr.s_addr=inet_addr(dest_ip_addr);	//specifies server IP address received from user
+	server_addr.sin_family=AF_INET;		// specifies family of addresses (IPv4)
+	server_addr.sin_port=htons(PORT);	// specifies port the server will be using 
+	server_addr.sin_addr.s_addr=inet_addr(dest_ip_addr);	//specifies server IP address received from user
 
-if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {	//if the connect function returns -1 (fails)
-	perror("[-] error connecting to server ");	//prints an appropriate error message
-}
-else {		//if the connect function returns 0 (succeeds)
-	printf("[+] successfully connected\n");
-	recv(client_socket, buffer, 256, 0);		//recieves message from server and stores it in buffer
-	printf("[+] Server : %s\n ", buffer);		//prints the server message stored in buffer
-
+	if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {	//if the connect function returns -1 (fails)
+		perror("[-] error connecting to server ");	//prints an appropriate error message
 	}
+	else {		//if the connect function returns 0 (succeeds)
+		printf("[+] successfully connected\n");
+		recv(client_socket, buffer, 256, 0);		//recieves message from server and stores it in buffer
+		printf("[+] Server : %s\n ", buffer);		//prints the server message stored in buffer
+	
+		}
+	
+	close(client_socket);		//closes the open socket connected to the server
 
-close(client_socket);		//closes the open socket connected to the server
-
-		return 0;
+	return 0;
 }
 
 int main(int argc, char** argv) {
@@ -53,5 +53,7 @@ else {		// if user has entered IP address when opening program
 	strcpy(dest_ip_addr, argv[1]);	//copy command line argument to 'dest_ip_addr' variable 
 }
 connect_to_server(dest_ip_addr);
+
+return 0;
 }
 	
